6d9e36d6b1fb3f72133c1794cd1d8c694cbca5c1
Wayvi2 <dmmaxime76@gmail.com>
2025-01-19 13:21:59 +0100

    add auto-completion for /wfly reload

--- src/main/java/com/wayvi/wfly/wflyV2/commands/ReloadCommand.java
+++ src/main/java/com/wayvi/wfly/wflyV2/commands/ReloadCommand.java
@@ -1,23 +1,28 @@
 package com.wayvi.wfly.wflyV2.commands;
 
+import com.wayvi.wfly.wflyV2.constants.Permissions;
 import fr.traqueur.commands.api.Arguments;
 import fr.traqueur.commands.api.Command;
+import fr.traqueur.commands.api.arguments.ArgumentConverter;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.wayvi.wfly.wflyV2.util.ConfigUtil;
 
-public class ReloadCommand extends Command<JavaPlugin> {
+import java.util.Arrays;
+import java.util.List;
+
+public class ReloadCommand extends Command<JavaPlugin>  {
 
     ConfigUtil configUtil;
     Plugin plugin;
 
 
     public ReloadCommand(JavaPlugin plugin, ConfigUtil configUtil) {
-        super(plugin, "wfly");
+        super(plugin, "wfly.reload");
         setDescription("Reload file of the plugin.");
         setUsage("/wfly reload");
-        addArgs("reload:string");
         this.plugin = plugin;
         this.configUtil = configUtil;
     }
@@ -31,4 +36,6 @@ public class ReloadCommand extends Command<JavaPlugin> {
         commandSender.sendMessage(message);
 
     }
+
+
 }
