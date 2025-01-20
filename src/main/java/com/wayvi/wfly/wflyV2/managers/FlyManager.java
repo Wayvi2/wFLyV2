68b2880e5d561873283be57dcb6c02e96c0b280a
Wayvi2 <dmmaxime76@gmail.com>
2025-01-20 20:51:11 +0100

    try to manage fall damage on disabled fly by command

--- src/main/java/com/wayvi/wfly/wflyV2/managers/FlyManager.java
+++ src/main/java/com/wayvi/wfly/wflyV2/managers/FlyManager.java
@@ -3,8 +3,13 @@ package com.wayvi.wfly.wflyV2.managers;
 import com.wayvi.wfly.wflyV2.services.DatabaseService;
 import com.wayvi.wfly.wflyV2.storage.AccessPlayerDTO;
 import fr.maxlego08.sarah.RequestHelper;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -22,6 +27,8 @@ public class FlyManager {
 
     private final RequestHelper requestHelper;
 
+    private BukkitTask flyTask;
+
     private DatabaseService databaseService;
 
 
@@ -34,17 +41,86 @@ public class FlyManager {
     }
 
     public void manageFly(Player player, boolean fly) {
-        player.setAllowFlight(fly);
+
+        upsertFlyStatus(player, fly);
+
+        if (fly) {
+            player.setAllowFlight(true);
+            player.setFlying(true);
+            if (flyTask != null && !flyTask.isCancelled()) {
+                flyTask.cancel();
+            }
+        } else {
+            player.setFlying(false);
+
+
+            flyTask = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+                if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
+                    player.setAllowFlight(false);
+                }
+            }, 20L, 20L);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Dan,s le cas fly = False
+On enleve le fly  et 1 tick plus tard on fait le calculer de quand enlever la permission
+
+/*
+                    new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        player.setAllowFlight(false);
+                        player.sendMessage("toto");
+                    }
+                }.runTaskLater(plugin, (long) Math.sqrt(2 * player.getFallDistance() / 0.08));
+            } else {
+                player.setAllowFlight(false);
+        }
         player.setFlying(fly);
+ */
+
+
+
+
+
+
+        /*
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.sendMessage(String.valueOf(player.getAllowFlight()));
+            }
+        }.runTaskTimer(plugin, 0, 20);
+  */
+
+
+    public AccessPlayerDTO getIsInFlyBeforeDeconnect(Player player) throws SQLException {
+
+        List<AccessPlayerDTO> fly = this.requestHelper.select("fly", AccessPlayerDTO.class, table -> table.where("uniqueId", player.getUniqueId()));
+
+        if (fly.isEmpty()) {
+            return new AccessPlayerDTO(player.getUniqueId(), false);
+        } else {
+            return fly.getFirst();
+
+        }
     }
-
-    public List<AccessPlayerDTO> getIsInFlyBeforeDeconnect(Player player) throws SQLException {
-        return this.requestHelper.select("fly", AccessPlayerDTO.class, table -> {
-            table.where("uniqueId", player.getUniqueId());
-
-        });
-    }
-
 
 
     public void upsertFlyStatus(Player player, boolean isFlying) {
