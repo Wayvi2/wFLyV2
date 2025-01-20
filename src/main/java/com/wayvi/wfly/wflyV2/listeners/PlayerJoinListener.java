574c7a4d23d2b1e919ac242825f686d10d84c032
Wayvi2 <dmmaxime76@gmail.com>
2025-01-20 20:50:35 +0100

    add PlayerJoinListener

---
+++ src/main/java/com/wayvi/wfly/wflyV2/listeners/PlayerJoinListener.java
@@ -0,0 +1,30 @@
+package com.wayvi.wfly.wflyV2.listeners;
+
+import com.wayvi.wfly.wflyV2.managers.FlyManager;
+import com.wayvi.wfly.wflyV2.storage.AccessPlayerDTO;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.sql.SQLException;
+
+public class PlayerJoinListener implements Listener {
+
+    FlyManager flyManager;
+
+    public PlayerJoinListener(FlyManager flyManager) {
+        this.flyManager = flyManager;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) throws SQLException {
+        Player player = event.getPlayer();
+        AccessPlayerDTO playerFlyData = flyManager.getIsInFlyBeforeDeconnect(player);
+
+        if (playerFlyData.isinFly()) {
+            flyManager.manageFly(player, true);
+        }
+    }
+
+}
