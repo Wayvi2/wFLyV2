56b8aea1cdd467be1ce05a77b4df7710aff8307d
Wayvi2 <dmmaxime76@gmail.com>
2025-01-29 17:12:30 +0100

    add spigot support minimessage

--- src/main/java/com/wayvi/wfly/wflyV2/util/MiniMessageSupportUtil.java
+++ src/main/java/com/wayvi/wfly/wflyV2/util/MiniMessageSupportUtil.java
@@ -11,14 +11,11 @@ public class MiniMessageSupportUtil {
 
 
         public static String translateToSpigot(String minimessage) {
-            // Utilisation d'un parser Minimessage pour convertir la chaîne en Component
-            Component component = net.kyori.adventure.text.minimessage.MiniMessage.miniMessage().deserialize(minimessage);
 
-            // Sérialisation du Component en texte formaté pour Spigot
+            Component component = net.kyori.adventure.text.minimessage.MiniMessage.miniMessage().deserialize(minimessage);
             return LegacyComponentSerializer.legacySection().serialize(component);
         }
 
-        // Méthode d'envoi du message converti à un joueur
         public static void sendMiniMessageFormat(Player player, String minimessage) {
             String spigotMessage = translateToSpigot(minimessage);
             player.sendMessage(spigotMessage);
