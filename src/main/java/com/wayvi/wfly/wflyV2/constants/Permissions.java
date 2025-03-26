72623cc08313a03dbb15fc8d55d530db272a0702
Wayvi2 <dmmaxime76@gmail.com>
2025-03-26 21:44:56 +0100

    add javadoc

--- src/main/java/com/wayvi/wfly/wflyV2/constants/Permissions.java
+++ src/main/java/com/wayvi/wfly/wflyV2/constants/Permissions.java
@@ -1,25 +1,68 @@
 package com.wayvi.wfly.wflyV2.constants;
 
+/**
+ * Enum representing the various permissions used in the WFly plugin.
+ * Each constant corresponds to a specific permission node for different functionalities in the plugin.
+ */
 public enum Permissions {
 
-
+    /**
+     * Permission to reload the plugin.
+     */
     RELOAD("wfly.reload"),
+
+    /**
+     * Permission to toggle fly mode for the player.
+     */
     FLY("wfly.fly"),
+
+    /**
+     * Permission to change the fly speed of the player.
+     */
     FLY_SPEED("wfly.fly.speed"),
+
+    /**
+     * Permission to add fly time to a player.
+     */
     ADD_FLY_TIME("wfly.add.time"),
+
+    /**
+     * Permission to remove fly time from a player.
+     */
     REMOVE_FLY_TIME("wfly.remove.time"),
+
+    /**
+     * Permission to reset the player's fly time.
+     */
     ADD_RESET_TIME("wfly.reset.time"),
+
+    /**
+     * Permission to enable infinite fly time for the player.
+     */
     INFINITE_FLY("wfly.infinite.fly"),
-    BYPASS_FLY("wfly.bypass.fly"),
-    ;
+
+    /**
+     * Permission to bypass the fly restrictions (e.g., for staff or admins).
+     */
+    BYPASS_FLY("wfly.bypass.fly");
 
     private final String permission;
 
+    /**
+     * Constructor for the Permissions enum. Each permission corresponds to a string value.
+     *
+     * @param permission the permission string
+     */
     Permissions(String permission) {
         this.permission = permission;
     }
 
-    public String getPermission(){
+    /**
+     * Gets the permission string associated with this enum constant.
+     *
+     * @return the permission string
+     */
+    public String getPermission() {
         return permission;
     }
 }
