a37edf74bba9a337f27a5aa56d3f502004d48930
Wayvi2 <dmmaxime76@gmail.com>
2025-05-21 13:53:26 +0200

    clarifing flyCommand priority

--- src/main/java/com/wayvi/wfly/wflyv2/util/ConfigUtil.java
+++ src/main/java/com/wayvi/wfly/wflyv2/util/ConfigUtil.java
@@ -26,7 +26,7 @@ public class ConfigUtil {
     private File configFile;
     private FileConfiguration configConfig;
 
-    private String version = "1.0.1.8";
+    private String version = "1.0.1.9";
     private final Plugin plugin;
 
     /**
