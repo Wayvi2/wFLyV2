ac5feb1931fab41e3dfa37aad660fb03ac240bc7
unknown <dmmaxime76@gmail.com>
2025-08-07 13:53:19 +0200

    add WExchangeManager

--- src/main/java/com/wayvi/wfly/wflyv2/managers/WExchangeManager.java
+++ src/main/java/com/wayvi/wfly/wflyv2/managers/WExchangeManager.java
@@ -1,4 +1,17 @@
 package com.wayvi.wfly.wflyv2.managers;
 
-public class WExchangeManager {
+import com.wayvi.wfly.wflyv2.api.ExchangeManager;
+import com.wayvi.wfly.wflyv2.api.WflyApi;
+import org.bukkit.entity.Player;
+
+import java.io.Externalizable;
+
+public class WExchangeManager implements ExchangeManager {
+
+
+    @Override
+    public void exchangeTimeFly(Player donator, Player receiver, int time) {
+        WflyApi.get().getTimeFlyManager().removeFlyTime(donator, time);
+        WflyApi.get().getTimeFlyManager().addFlytime(receiver, time);
+    }
 }
